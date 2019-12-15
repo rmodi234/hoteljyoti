40044f981c003c24959e26bbc215d416e8261cc9
rmodi234 <rahul.modi@neologicx.com>
2019-12-15 16:05:16 +0530

    Updates

---
+++ precache-manifest.47a21927d6f32f2bd3a62163bd1280af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7f75f4be210c98ecb587625e9c9314",
+    "url": "/hoteljyoti/index.html"
+  },
+  {
+    "revision": "be5fe9e96fd39cb7036b",
+    "url": "/hoteljyoti/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d4d1f59c4ed1f7da1a7d",
+    "url": "/hoteljyoti/static/js/2.e870e464.chunk.js"
+  },
+  {
+    "revision": "7f99e0e51f1c89597c0cc7d370e10865",
+    "url": "/hoteljyoti/static/js/2.e870e464.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be5fe9e96fd39cb7036b",
+    "url": "/hoteljyoti/static/js/main.b861be5e.chunk.js"
+  },
+  {
+    "revision": "df49f1cf88cc22503ac4",
+    "url": "/hoteljyoti/static/js/runtime-main.ca636036.js"
+  },
+  {
+    "revision": "7d194181e99bfd23ba56fe7427a33c0e",
+    "url": "/hoteljyoti/static/media/bg1.7d194181.png"
+  },
+  {
+    "revision": "5cf024bbf89a5c6cdaa9b7e60d7467b1",
+    "url": "/hoteljyoti/static/media/logo.5cf024bb.png"
+  }
+]);
